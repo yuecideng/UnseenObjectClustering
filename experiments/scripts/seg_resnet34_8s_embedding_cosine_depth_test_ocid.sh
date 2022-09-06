1840152d5b2c3f24b6087302c28bfeb0aad49663
yuecideng <dengyueci@qq.com>
2022-09-06 20:05:24 +0800

    change ply to o3d

--- experiments/scripts/seg_resnet34_8s_embedding_cosine_depth_test_ocid.sh
+++ experiments/scripts/seg_resnet34_8s_embedding_cosine_depth_test_ocid.sh
@@ -7,7 +7,7 @@ export CUDA_VISIBLE_DEVICES=$1
 
 ./tools/test_net.py \
   --network seg_resnet34_8s_embedding \
-  --pretrained output/tabletop_object/tabletop_object_train/seg_resnet34_8s_embedding_cosine_depth_sampling_epoch_$2.checkpoint.pth  \
+  --pretrained data/checkpoints/seg_resnet34_8s_embedding_cosine_depth_sampling_epoch_$2.checkpoint.pth  \
   --dataset ocid_object_test \
   --cfg experiments/cfgs/seg_resnet34_8s_embedding_cosine_depth_tabletop.yml \
-  --pretrained_crop output/tabletop_object/tabletop_object_train/seg_resnet34_8s_embedding_cosine_depth_crop_sampling_epoch_$3.checkpoint.pth  \
+  --pretrained_crop data/checkpoints/seg_resnet34_8s_embedding_cosine_depth_crop_sampling_epoch_$3.checkpoint.pth  \
